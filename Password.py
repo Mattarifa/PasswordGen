f60de431fbcad02e889c98ab406d16383c281c44
Matias <matiastarifa4@gmail.com>
2024-07-01 18:02:30 -0300

    robustez

---
+++ Password.py
@@ -0,0 +1,15 @@
+import random 
+
+minus="abcdefghijklmnopqrstuvwxyz"
+mayus=minus.upper()
+numeros="0123456789"
+simbolos="@()[]{}*,:/-_?.!#$%^&*()+=|"
+
+base=minus+mayus+numeros+simbolos
+
+longitud= 12
+
+muestra= random.sample(base,longitud)
+password="".join(muestra) # str vacio para utilizar el metodo .join con objeto iterable
+print(password)
+
