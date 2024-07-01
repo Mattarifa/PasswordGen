925cf3b89eec0e93b3be3d14cc5037fe1c5da838
Matias <matiastarifa4@gmail.com>
2024-07-01 18:48:27 -0300

    hash implementado

--- Password.py
+++ Password.py
@@ -1,4 +1,5 @@
 import random 
+from werkzeug.security import generate_password_hash
 
 minus="abcdefghijklmnopqrstuvwxyz"
 mayus=minus.upper()
@@ -13,5 +14,7 @@ longitud= 12
 for _ in range(10):
     muestra= random.sample(base,longitud)
     password="".join(muestra) # str vacio para utilizar el metodo .join con objeto iterable
-    print(password)
+    password_encriptado=generate_password_hash(password)
+
+    print("{} =>    {}".format(password,password_encriptado))
 
